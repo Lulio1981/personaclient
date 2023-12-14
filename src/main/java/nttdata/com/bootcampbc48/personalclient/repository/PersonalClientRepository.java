ffe8e264a04e4f21924806c469820fb82f8c846e
lulio1981 <lulio1981@gmail.com>
2023-12-13 19:43:56 -0500

    docker file docker compose

--- src/main/java/nttdata/com/bootcampbc48/personalclient/repository/PersonalClientRepository.java
+++ src/main/java/nttdata/com/bootcampbc48/personalclient/repository/PersonalClientRepository.java
@@ -4,14 +4,15 @@ import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Maybe;
 import nttdata.com.bootcampbc48.personalclient.entity.PersonalClient;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-@Repository
+@EnableReactiveMongoRepositories
 public interface PersonalClientRepository extends ReactiveMongoRepository<PersonalClient, String> {
 
-    public Mono<PersonalClient> findById(String id);
+    Mono<PersonalClient> findById(String id);
 
-    public Mono<PersonalClient> findByDocumentNumber(String documentNumber);
+    Mono<PersonalClient> findByDocumentNumber(String documentNumber);
 
 }
